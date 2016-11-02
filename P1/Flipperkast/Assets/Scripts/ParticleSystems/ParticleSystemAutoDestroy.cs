94857fe1ce95e312ac19ddfdb811797b05ff80a8
JarnoVosDeltion <jarno908@hotmail.com>
2016-11-02 17:53:38 +0100

    Grote Update
    Alle Meshes, Textures en Scripts zitten er nu in.

---
+++ P1/Flipperkast/Assets/Scripts/ParticleSystems/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour
+{
+    ParticleSystem ps;
+
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
