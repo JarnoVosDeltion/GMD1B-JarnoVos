57fd468829069d37ca582ed5af8394f5307fa07a
JarnoVosDeltion <jarno908@hotmail.com>
2016-11-04 09:57:10 +0100

    Reinitialized Project

---
+++ P1/Flipperkast/Assets/Scripts/UI/BallsLeft.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallsLeft : MonoBehaviour {
+
+    public int ballsStarting = 2;
+    public string ballsLeftText;
+    int ballsLeft;
+    bool allowPinballSpawn = true;
+    Text textBallsLeft;
+    PinballSpawn pinballSpawnScript;
+    CanvasGroup canvasOverlayGroup;
+
+	void Start()
+    {
+        canvasOverlayGroup = GameObject.Find("CanvasOverlay").GetComponent<CanvasGroup>();
+        textBallsLeft = GameObject.Find("BallsLeftText").GetComponent<Text>();
+        pinballSpawnScript = GetComponent<PinballSpawn>();
+
+        ballsLeft = ballsStarting;
+        textBallsLeft.text = ballsLeftText + (ballsLeft + 1);
+	}
+
+    void Update()
+    {
+        if (allowPinballSpawn)
+        {
+            if (Input.GetButtonDown("Vertical"))
+            {
+                pinballSpawnScript.SpawnPinball();
+                allowPinballSpawn = false;
+                textBallsLeft.text = ballsLeftText + ballsLeft;
+            }
+        }
+    }
+
+    public void ChangingBalls(int change, bool newBall = false)
+    {
+        ballsLeft += change;
+        if(ballsLeft < 0)
+        {
+            canvasOverlayGroup.alpha = 1;
+        }
+        else
+        {
+            if (newBall)
+            {
+                allowPinballSpawn = newBall;
+            }
+
+            else
+            {
+                textBallsLeft.text = ballsLeftText + ballsLeft;
+            }
+        }
+
+    }
+	
+}
---
+++ P1/Flipperkast/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public string textScore;
+    int score = 0;
+    Text scoreText;
+
+    void Start()
+    {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = textScore + score;
+    }
+	
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = textScore + score;
+    }
+}
